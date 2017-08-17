0963c72bb90a6ed4b76f7d5545d98b721923aeb5
swidilator <kyleweiher@gmail.com>
2017-08-17 08:28:08 +0200

    signup and login working, need some data validation

--- app/src/main/java/com/software/design/realestateapp/MainActivity.java
+++ app/src/main/java/com/software/design/realestateapp/MainActivity.java
@@ -8,11 +8,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     //initialize variables
     Button signUp;
     Button login;
     EditText username, password;
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +43,98 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //assign text fields to variables
+        username = (EditText)findViewById(R.id.editText_Username_Login);
+        password = (EditText)findViewById(R.id.editText_Password_Login);
+        url = "http://lamp.ms.wits.ac.za/~s1037363/realestate_app/existsUser.php";
+
         login = (Button)findViewById(R.id.button_login);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                username = (EditText)findViewById(R.id.editText_Username_Login);
-                password = (EditText)findViewById(R.id.editText_Password_Login);
-                Toast toast = Toast.makeText(getApplicationContext(), username.getText().toString()+" "+ password.getText().toString(), Toast.LENGTH_LONG);
-                toast.show();
-            }
-        });
     }
+
+    //method that returns values based on the servers response
+    public int processJSON(String json) {
+        System.out.println(json);
+        if (json.contains("1")) {
+            return 1;
+        } else{
+            return 0;
+        }
+    }
+
+    public void loginUser(){
+        final String usernameData = username.getText().toString().trim();
+        final String passwordData = password.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //Toast.makeText(MainActivity.this,response,Toast.LENGTH_LONG).show();
+                        int result = processJSON(response);
+
+                        if(result==1){
+                            System.out.println("result is: " + result);
+                            Toast.makeText(getApplicationContext(),"Existing found", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(getApplicationContext(),"No user found", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("USERNAME",usernameData);
+                params.put("PASSWORD",passwordData);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    public void onLoginClick(View v){
+        loginUser();
+
+    }
+
+
+//    public void loginClick(View v) {
+//        ArrayList<String[]> list = new ArrayList<>();
+//        String user = username.getText().toString();
+//        String pass = password.getText().toString();
+//        if (!user.isEmpty() && !pass.isEmpty()) {
+//            list.add(new String[]{"username", user});
+//            list.add(new String[]{"password", pass});
+//
+//            AsyncHttpPost asyncHttpPost = new AsyncHttpPost(new AsyncHandler() {
+//
+//                @Override
+//                public void handleResponse(String response) {
+//
+//                    int result = processJSON(response);
+//
+//                    if (result == 1) {
+//                        Toast toast = Toast.makeText(getApplicationContext(), "Existing user", Toast.LENGTH_LONG);
+//                        toast.show();
+//                    } else {
+//                        Toast toast = Toast.makeText(getApplicationContext(), "Unknown username", Toast.LENGTH_LONG);
+//                        toast.show();
+//                    }
+//
+//
+//                }
+//
+//            });
+//            asyncHttpPost.execute("http://lamp.ms.wits.ac.za/~s1037363/realestate_app/test.php", list);
+//        }
+//    }
 }
--- app/src/main/java/com/software/design/realestateapp/SignUpActivity.java
+++ app/src/main/java/com/software/design/realestateapp/SignUpActivity.java
@@ -2,12 +2,101 @@ package com.software.design.realestateapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //declare on screen element variables
+    Button signUp;
+    EditText name, surname, password, confirmPassword, username, email, phonenumber;
+    CheckBox agent;
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        //assign screen element variables to fields
+        name = (EditText)findViewById(R.id.editText_Name);
+        surname = (EditText)findViewById(R.id.editText_Surname);
+        password = (EditText)findViewById(R.id.editText_Password);
+        confirmPassword = (EditText)findViewById(R.id.editText_ConfirmPassword);
+        username = (EditText)findViewById(R.id.editText_Username);
+        email = (EditText)findViewById(R.id.editText_EmailAddress);
+        phonenumber = (EditText)findViewById(R.id.editText_PhoneNumber);
+        agent = (CheckBox)findViewById(R.id.checkBox_Agent);
+        signUp = (Button)findViewById(R.id.button_SignUpSend);
+        //php url for insert
+        url = "http://lamp.ms.wits.ac.za/~s1037363/realestate_app/insertUser.php";
+    }
+
+    //on click method for signup button
+    public void signUpUser(View v) {
+        //get current values from fields
+        final String usernameData = username.getText().toString().trim();
+        final String passwordData = password.getText().toString().trim();
+        final String nameData = name.getText().toString().trim();
+        final String surnameData = surname.getText().toString().trim();
+        final String confirmPasswordData = confirmPassword.getText().toString().trim();
+        final String phonenumberData = phonenumber.getText().toString().trim();
+        final String emailData = email.getText().toString().trim();
+        final boolean agentBool = agent.isChecked();
+        final String agentData;
+        if(agentBool){
+            agentData = "A";
+        }else{
+            agentData = "R";
+        }
+        //if passwords match send data for query
+        if(passwordData.equals(confirmPasswordData)){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG).show();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("USERNAME", usernameData);
+                params.put("PASSWORD", passwordData);
+                params.put("NAME", nameData);
+                params.put("SURNAME", surnameData);
+                params.put("EMAIL", emailData);
+                params.put("PHONENUMBER", phonenumberData);
+                params.put("USER_TYPE", agentData);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }else{
+            System.out.println("The passwords do not match");
+            Toast.makeText(getApplicationContext(),"The passwords do not match!",Toast.LENGTH_LONG).show();
+        }
     }
 }
