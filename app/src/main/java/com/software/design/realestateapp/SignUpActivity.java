c0eb64b076b69ce213ea4b11091d09404ed133c6
Your Name1307731 <1307731@students.wits.ac.za>
2017-10-10 14:07:27 +0200

    Fixed up a number of forms, moved some methods

--- app/src/main/java/com/software/design/realestateapp/SignUpActivity.java
+++ app/src/main/java/com/software/design/realestateapp/SignUpActivity.java
@@ -127,64 +127,145 @@ public class SignUpActivity extends AppCompatActivity {
         }
 
         //Error validation
-        //if passwords match send data for query
-        if (checkCompletedFields(usernameData, passwordData, nameData, surnameData, confirmPasswordData, phonenumberData, emailData) == 0) {
 
-            if (passwordData.equals(confirmPasswordData)) {
-
-
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                resultTextView.setText(response);
-
-
-                                //Success
-                                if (response.equals("0")) {
-                                    Toast.makeText(getApplicationContext(), getString(R.string.SignUp_CreatedUser_0), Toast.LENGTH_LONG).show();
-                                } else if (response.equals("1")) {
-                                    Toast.makeText(getApplicationContext(), getString(R.string.SignUp_Failed_1), Toast.LENGTH_LONG).show();
-                                }
-
-                                //Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
-
-
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                resultTextView.setText("2");
-                                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
-                            }
-                        }) {
-                    @Override
-                    protected Map<String, String> getParams() {
-                        Map<String, String> params = new HashMap<String, String>();
-                        params.put("USERNAME", usernameData);
-                        params.put("PASSWORD", passwordData);
-                        params.put("NAME", nameData);
-                        params.put("SURNAME", surnameData);
-                        params.put("EMAIL", emailData);
-                        params.put("PHONENUMBER", phonenumberData);
-                        params.put("USER_TYPE", agentData);
-                        return params;
-                    }
-
-                };
-
-                RequestQueue requestQueue = Volley.newRequestQueue(this);
-                requestQueue.add(stringRequest);
-            } else {
-                //Passwords do not match
-                resultTextView.setText("3");
-                Toast.makeText(getApplicationContext(), getString(R.string.SignUp_PasswordNoMatch_3), Toast.LENGTH_LONG).show();
-            }
-        } else {
+        boolean valid = true;
+        //check if all fields are complete
+        if (checkCompletedFields(usernameData, passwordData, nameData, surnameData, confirmPasswordData, phonenumberData, emailData) != 0)
+        {
+            valid=false;
             resultTextView.setText("4");
             Toast.makeText(getApplicationContext(), getString(R.string.SignUp_FieldsIncomplete_4), Toast.LENGTH_LONG).show();
         }
+
+        //checks if passwords match
+
+        if (!passwordData.equals(confirmPasswordData))
+        {
+                //Passwords do not match
+                valid=false;
+                resultTextView.setText("3");
+                Toast.makeText(getApplicationContext(), getString(R.string.SignUp_PasswordNoMatch_3), Toast.LENGTH_LONG).show();
+        }
+
+        //checks if email is valid
+        if(!emailData.contains("@"))
+        {
+            //checks if email has a @ symbol
+            valid=false;
+            resultTextView.setText("1");
+            Toast.makeText(getApplicationContext(), "Email is invalid", Toast.LENGTH_LONG).show();
+        }
+
+        //checks if number contains only digits, android should cause this by default
+        if(!checkNumber(phonenumberData))
+        {
+            //checks if phone has only digits
+            valid=false;
+            resultTextView.setText("1");
+            Toast.makeText(getApplicationContext(), "Phone number is invalid", Toast.LENGTH_LONG).show();
+        }
+
+        if(!checkName(nameData))
+        {
+            valid=false;
+            resultTextView.setText("1");
+            Toast.makeText(getApplicationContext(), "Please make sure there are no numbers in the name", Toast.LENGTH_LONG).show();
+        }
+
+        if(!checkName(surnameData))
+        {
+            valid=false;
+            resultTextView.setText("1");
+            Toast.makeText(getApplicationContext(), "Please make sure there are no numbers in the name", Toast.LENGTH_LONG).show();
+        }
+
+        //creates user if valid
+        if(valid)
+        {
+
+            resultTextView.setText("0");
+            Toast.makeText(getApplicationContext(),"Adding user now", Toast.LENGTH_LONG).show();
+            createUser(usernameData, passwordData, nameData, surnameData, confirmPasswordData, phonenumberData, emailData, agentData);
+        }
+
+
+    }
+
+    public boolean checkName(String name)
+    {
+        for (int i = 0; i < name.length(); i++)
+        {
+            if(Character.isDigit(name.charAt(i)))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean checkNumber(String number)
+    {
+        for (int i = 0; i < number.length(); i++)
+        {
+            if(!Character.isDigit(number.charAt(i)))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void createUser(String usernameData, String passwordData, String nameData, String surnameData, String confirmPasswordData, String phonenumberData, String emailData, String agentData)
+    {
+
+        final String c_usernameData=usernameData, c_passwordData= passwordData, c_nameData=nameData, c_surnameData=surnameData, c_confirmPasswordData=confirmPasswordData, c_phonenumberData=phonenumberData, c_emailData=emailData, c_agentData= agentData;
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        resultTextView.setText(response);
+
+
+                        //Success
+                        if (response.equals("0")) {
+                            Toast.makeText(getApplicationContext(), getString(R.string.SignUp_CreatedUser_0), Toast.LENGTH_LONG).show();
+                        } else if (response.equals("1")) {
+                            Toast.makeText(getApplicationContext(), getString(R.string.SignUp_Failed_1), Toast.LENGTH_LONG).show();
+                        }
+
+                        //Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        resultTextView.setText("2");
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("USERNAME", c_usernameData);
+                params.put("PASSWORD", c_passwordData);
+                params.put("NAME", c_nameData);
+                params.put("SURNAME", c_surnameData);
+                params.put("EMAIL", c_emailData);
+                params.put("PHONENUMBER", c_phonenumberData);
+                params.put("USER_TYPE", c_agentData);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
 
