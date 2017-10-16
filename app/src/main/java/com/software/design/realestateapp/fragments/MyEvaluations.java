23ad06b1bbff653d88781c64f59641ef5de917e9
MattMCollins <1178399@students.wits.ac.za>
2017-10-16 12:59:53 +0200

    Added past evaluations list

--- app/src/main/java/com/software/design/realestateapp/fragments/MyEvaluations.java
+++ app/src/main/java/com/software/design/realestateapp/fragments/MyEvaluations.java
@@ -10,13 +10,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.software.design.realestateapp.EvaluationActivity;
 import com.software.design.realestateapp.R;
 import com.software.design.realestateapp.RoomActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyEvaluations extends Fragment implements View.OnClickListener{
+
+    List<String> addresses = new ArrayList<String>();
+    List<String> suburb = new ArrayList<String>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,15 +34,59 @@ public class MyEvaluations extends Fragment implements View.OnClickListener{
         Button evalBtn = (Button) v.findViewById(R.id.addEvaluationBtn);
         evalBtn.setOnClickListener(this);
 
+        //Hard coded for now, will get data from database
+        addresses.add("5 Janet Street");
+        suburb.add("Glenvista");
+        addresses.add("36 Basson Drive");
+        suburb.add("Glenvista");
+        addresses.add("1 Jan Smuts Avenue");
+        suburb.add("Braamfontein");
+        addresses.add("44 Stanley Avenue");
+        suburb.add("Braamfontein");
+        addresses.add("45 Vorster Avenue");
+        suburb.add("Glenvista");
+
+        LinearLayout ll = (LinearLayout) v.findViewById(R.id.pastEvaluationLayout);
+
+        for(int i = 0; i < addresses.size(); i++) {
+            RelativeLayout rl = (RelativeLayout) getActivity().getLayoutInflater().
+                    inflate(R.layout.past_evaluations, null);
+
+            TextView t;
+            t = (TextView) rl.findViewById(R.id.idText);
+            t.setText(""+(i+1));
+
+            t = (TextView) rl.findViewById(R.id.addressText);
+            t.setText(addresses.get(i));
+
+            t = (TextView) rl.findViewById(R.id.suburbText);
+            t.setText(suburb.get(i));
+
+            rl.setOnClickListener(this);
+            //id will match number when on list
+            rl.setId(i+1);
+            ll.addView(rl);
+        }
+
         // Inflate the layout for this fragment
         return v;
     }
 
     @Override
     public void onClick(View v){
-        ///Toast.makeText(getActivity(),"Button Working",Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(getActivity(), EvaluationActivity.class);
-        startActivity(intent);
+
+        switch (v.getId()){
+            case R.id.addEvaluationBtn:
+                ///Toast.makeText(getActivity(),"Button Working",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getActivity(), EvaluationActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                Toast.makeText(getActivity(),"Placeholder for evaluation "+ v.getId(),Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
     }
 
 
