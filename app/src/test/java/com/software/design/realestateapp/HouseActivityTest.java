f002f043328f6604f07f92310d90dd0390c70995
kyle <kyleweiher@gmail.com>
2017-10-29 10:03:51 +0200

    Initail HouseActivity test

---
+++ app/src/test/java/com/software/design/realestateapp/HouseActivityTest.java
@@ -0,0 +1,25 @@
+package com.software.design.realestateapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by kyle on 2017/10/29.
+ */
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class HouseActivityTest {
+
+    @Test
+    public void onCreate() throws Exception {
+
+        HouseActivity t = Robolectric.setupActivity(HouseActivity.class);
+
+        assertNotNull(t);
+    }
+}
